aa838f614937250d048657d8562fa010d953c1bd
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-08 10:47:21 -0700

    📅  | Tue Mar  8 10:47:21 MST 2022

---
+++ max-angular-material-tutorial/src/app/matrial.module.ts
@@ -0,0 +1,8 @@
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [MatButtonModule],
+  exports: [MatButtonModule],
+})
+export class MaterialModule {}
